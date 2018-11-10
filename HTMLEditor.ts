7d0f698e5f0b93d4f9960ec754da272cf60760ef
Alexandr Plokhih <s.plohoj@gmail.com>
2018-11-10 22:45:40 +0300

    Refactorin whenLocation and rename to whenURL

--- HTMLEditor.ts
+++ HTMLEditor.ts
@@ -1,53 +1,28 @@
-import { Subject, Observable, of, OperatorFunction, pipe } from 'rxjs';
-import { delayWhen, filter, switchMap, merge, takeUntil, delay, tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { delay, distinctUntilChanged, filter, map, skip, switchMap, takeUntil, tap, windowCount } from 'rxjs/operators';
 
-export const historyChangeSubject = new Subject<string>();
-document.addEventListener('hashchange', () => historyChangeSubject.next(location.href));
+export const historyChange = new BehaviorSubject(location.href);
+document.addEventListener('hashchange', () => historyChange.next(location.href));
 const pushState = history.pushState;
-history.pushState = function () {
-    pushState.apply(history, arguments);
-    historyChangeSubject.next(location.href);
+history.pushState = function (...args) {
+    pushState.apply(history, args);
+    if (historyChange.value !== location.href) {
+        historyChange.next(location.href);
+    }
 };
 
-export function whenLocation(href: RegExp, flow: Observable<any>): Observable<string> {
-    let isCoincidenceLast = false;
-    let observableEmitter: Observable<any>;
-    let observable = of(null)
-        .pipe(
-            delayWhen(() => historyChangeSubject
-                .pipe(
-                    filter(value => {
-                        if (href.test(value)) {
-                            if (isCoincidenceLast) {
-                                return false;
-                            } else {
-                                isCoincidenceLast = true;
-                                return true;                    
-                            }
-                        }
-                        isCoincidenceLast = false;
-                        return false;
-                    })
-                )
-            ),
-            switchMap(() => observableEmitter)
-        )
-    observableEmitter = observable.pipe(
-        merge(
-            flow.pipe(
-                takeUntil(historyChangeSubject
-                    .pipe(
-                        filter(value => href.test(value))
-                    )
-                )
-            ),
-        )
-    )
-    if (href.test(location.href)) {
-        isCoincidenceLast = true;
-        return observableEmitter;
-    }
-    return <Observable<string>>observable;
+export function whenURL(href: RegExp): Observable<Observable<string>> {
+    return historyChange.pipe(
+        distinctUntilChanged(),
+        filter(url => href.test(url)),
+        windowCount(1),
+        skip(1),
+        map(() => historyChange.pipe(
+            takeUntil(historyChange.pipe(
+                filter(url => !href.test(url)),
+            )),
+        )),
+    );
 }
 
 export function waitHTMLElement(query: string, queryRepeatDelay = 200): Observable<HTMLElement> {
@@ -87,7 +62,7 @@ export function waitFromSetValue(query: string, value: string) {
         .pipe(
             tap((element: HTMLInputElement) => {
                 element.value = value;
-                element.dispatchEvent(new Event('input', <EventInit>{target: element}));
+                element.dispatchEvent(new Event('input', <EventInit>{ target: element }));
                 console.log(`I AM SET VALUE: "`, value, '" FROM', element);
             })
         );
